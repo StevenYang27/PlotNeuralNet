e62771d65b45ac7fbd1197f0a87c68db358b8f7d
StevenYang27 <jeanjacques2772@gmail.com>
2020-05-24 09:10:06 +0800

    First layer

--- my_project/sinet.py
+++ my_project/sinet.py
@@ -4,12 +4,20 @@ sys.path.append('../')
 from pycore.tikzeng import *
 
 arch = [
-    to_head(),
+    to_head('..'),
     to_cor(),
     to_begin(),
 
     #Input
-    to_input('Number.png'),
+    to_input('./Number.png'),
+
+    #Conv
+    to_ConvRelu('conv1', s_filer=28, n_filer=32, offset='(0, 0, 0)', to='(0, 0, 0)', height=64, depth=64, width=2),
+    
+
+
+    to_end()
+
 ]
 
 def main():
