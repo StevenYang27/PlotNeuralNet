5c868aef803331f4d23abe6b597a3f99a7661d89
StevenYang27 <jeanjacques2772@gmail.com>
2020-05-26 22:28:44 +0800

    Final update

--- my_project/sinet.py
+++ my_project/sinet.py
@@ -8,13 +8,23 @@ arch = [
     to_cor(),
     to_begin(),
 
-    #Input
-    to_input('./Number.png'),
+    # Picture
+    to_input('./Number.png', width=18, height=18),
 
-    #Conv
-    to_ConvRelu('conv1', s_filer=28, n_filer=32, offset='(0, 0, 0)', to='(0, 0, 0)', height=64, depth=64, width=2),
-    
+    # Convolution
+    to_ConvRelu('cr1', s_filer=28, n_filer=6, offset='(0, 0, 0)', to='(0, 0, 0)', height=64, depth=64, width=2, caption='conv1'),
+    to_Pool('pool1', offset='(0, 0, 0)', to='(cr1-east)', height=32, depth=32),
+    to_ConvRelu('cr2', s_filer=10, n_filer=6, offset='(2, 0, 0)', to='(pool1-east)', height=32, depth=32, width=2, caption='conv2'),
+    to_connection('pool1', 'cr2'),
+    to_Pool('pool2', offset='(0, 0, 0)', to='(cr2-east)', height=16, depth=16),
 
+    # Full Connection
+    to_FullConnection('fc1', s_filer=1, n_nueron=1024, offset='(4, 0, 0)', to='(pool2-east)', caption='fc1'),
+    to_connection('pool2', 'fc1'),
+    to_connection_dot('fc1', 'pool2'),
+    to_FullConnection('fc2', s_filer=1, n_nueron=10, offset='(1, 0, 0)', to='(fc1-east)', depth=30, caption='fc2'),
+    to_connection('fc1', 'fc2'),
+    to_connection_end('fc2'),
 
     to_end()
 
